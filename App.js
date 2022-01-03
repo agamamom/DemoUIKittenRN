d6a990a7cd55707d0220a3ca16f2762331bebb01
dAtdorable <dathoang31082001@gmail.com>
2022-01-03 11:54:37 +0700

    update app

--- App.js
+++ App.js
@@ -1,4 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import * as eva from '@eva-design/eva';
+
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+
+import { AppNavigator } from './navigation/AppNavigator';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </ApplicationProvider>
+  </>
+);
+
+// *****Default Code****
+/* import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -18,3 +40,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ */
+
+
+
+
+
+
+
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
